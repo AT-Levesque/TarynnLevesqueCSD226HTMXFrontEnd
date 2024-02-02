dc032c2c5a16049b622903235304d91e9dafd865
fcarella <fred.carella@gmail.com>
2024-02-02 11:05:22 -0500

    commit

--- src/main/java/csd226/lab2/controllers/AccountController.java
+++ src/main/java/csd226/lab2/controllers/AccountController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import csd226.lab2.data.*;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -28,8 +29,36 @@ public class AccountController {
     @Autowired
     JwtTokenUtil jwtUtil;
 
-    @PostMapping("/auth/login")
-    public ResponseEntity<?> login(@RequestBody @Valid AuthRequest request) {
+    @PostMapping("/test_form")
+    public String test_form(@ModelAttribute Account account, Model model) {
+        model.addAttribute("email", account);
+        return "result";
+    }
+    @PostMapping(path="/auth/login")
+    public ResponseEntity<?> login(@ModelAttribute Account acc, Model model) {
+        try {
+            Authentication authentication = authManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            acc.getEmail(), acc.getPassword())
+            );
+
+//            Account account = new Account();
+//            account.setId(1);
+//            account.setEmail(authentication.getPrincipal().toString());
+
+            Account account = (Account) authentication.getPrincipal();
+            String accessToken = jwtUtil.generateAccessToken(account);
+
+            AuthResponse response = new AuthResponse(account.getEmail(), accessToken);
+
+            return ResponseEntity.ok().body(response);
+        } catch( Exception ex) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+
+    @PostMapping(path="/auth/login_old")
+    public ResponseEntity<?> loginOld(@RequestBody @Valid AuthRequest request) {
         try {
             Authentication authentication = authManager.authenticate(
                     new UsernamePasswordAuthenticationToken(
@@ -76,15 +105,16 @@ public class AccountController {
 
     @GetMapping("/login")
     public ResponseEntity<String> getLogin(){ // map a URL to a method
-        String s="<script src=\"https://unpkg.com/htmx.org@1.9.10\" integrity=\"sha384-D1Kt99CQMDuVetoL1lrYwg5t+9QdHe7NLX/SoJYkXDFfX37iInKRy5xLSi8nO7UC\" crossorigin=\"anonymous\"></script>"+
-                "<div hx-get=\"/protectedPage\" hx-target=\"#protectedContent\">" +
-                "    Get protected content" +
-                "</div>" +
-                "<div id=\"protectedContent\"></div>" +
-                "<div hx-get=\"/publiccontent\" hx-target=\"#content\">" +
-                "    Get content" +
-                "</div>" +
-                "<div id=\"content\"></div>" +
+        String s=
+//                +"<script src=\"https://unpkg.com/htmx.org@1.9.10\" integrity=\"sha384-D1Kt99CQMDuVetoL1lrYwg5t+9QdHe7NLX/SoJYkXDFfX37iInKRy5xLSi8nO7UC\" crossorigin=\"anonymous\"></script>"+
+//                "<div hx-get=\"/protectedPage\" hx-target=\"#protectedContent\">" +
+//                "    Get protected content" +
+//                "</div>" +
+//                "<div id=\"protectedContent\"></div>" +
+//                "<div hx-get=\"/publiccontent\" hx-target=\"#content\">" +
+//                "    Get content" +
+//                "</div>" +
+//                "<div id=\"content\"></div>" +
                 "<form hx-post=\"/signup\" hx-target=\"this\" hx-swap=\"outerHTML\">" +
                 "    <div>" +
                 "        <label>Username</label>" +
@@ -104,23 +134,23 @@ public class AccountController {
         String s="<form hx-post=\"/auth/login\" hx-target=\"this\" hx-swap=\"outerHTML\">\n" +
                 "    <div>\n" +
                 "        <label>First Name</label>\n" +
-                "        <input type=\"text\" name=\"firstname\" value=\"Joe\">\n" +
+                "        <input type=\"text\" name=\"firstname\" value=\"Fred\">\n" +
                 "    </div>\n" +
                 "    <div class=\"form-group\">\n" +
                 "        <label>Last Name</label>\n" +
-                "        <input type=\"text\" name=\"lastname\" value=\"Blow\">\n" +
+                "        <input type=\"text\" name=\"lastname\" value=\"Carella\">\n" +
                 "    </div>\n" +
                 "    <div class=\"form-group\">\n" +
                 "        <label>Email Address</label>\n" +
-                "        <input type=\"email\" name=\"email\" value=\"joe@blow.com\">\n" +
+                "        <input type=\"email\" name=\"email\" value=\"fred.carella@gmail.com\">\n" +
                 "    </div>\n" +
                 "    <div class=\"form-group\">\n" +
                 "        <label>Password</label>\n" +
-                "        <input type=\"password\" name=\"password\" value=\"xxxxx\">\n" +
+                "        <input type=\"password\" name=\"password\" value=\"fredspassword\">\n" +
                 "    </div>\n" +
                 "    <div class=\"form-group\">\n" +
                 "        <label>Confirm Password</label>\n" +
-                "        <input type=\"password\" name=\"confirmPassword\" value=\"xxxxx\">\n" +
+                "        <input type=\"password\" name=\"confirmPassword\" value=\"fredspassword\">\n" +
                 "    </div>\n" +
                 "    <button class=\"btn\">Submit</button>\n" +
                 "    <button class=\"btn\" hx-get=\"/signin\">Cancel</button>\n" +
